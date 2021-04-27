c74c60052f9abd6f312f3ecf0d2e1ec24c6298ca
tbal999 <53711814+tbal999@users.noreply.github.com>
2021-04-27 08:49:41 +0100

    Update examples.go

--- examples/examples.go
+++ examples/examples.go
@@ -15,16 +15,22 @@ type ExampleOne struct {
 }
 
 func (example ExampleOne) sayHi() {
-	fmt.Println("hello")
+	fmt.Println("example 1 says hello")
 	fmt.Println(example.thing1, example.thing2, example.thing3)
 }
 
-func (example ExampleOne) sayBye() {
-	fmt.Println("bye")
+type ExampleTwo struct {
+	thing1 string
+	thing2 string
+	thing3 string
+}
+
+func (example ExampleTwo) sayHi() {
+	fmt.Println("example 2 says hello")
 	fmt.Println(example.thing1, example.thing2, example.thing3)
 }
 
-func (example ExampleOne) sayWhy() {
-	fmt.Println("why")
-	fmt.Println(example.thing1, example.thing2, example.thing3)
+func Hi(g geometry) {
+	g.sayHi()
 }
+
